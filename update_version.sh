4b52861466db0ba5d23c7e804e73ad414353f8f9
Mathieu Agopian <mathieu@agopian.info>
2017-09-08 14:26:19 +0200

    Don't track version.json changes, move it to the src/ folder

--- update_version.sh
+++ update_version.sh
@@ -2,5 +2,5 @@ NAME="Delivery Dashboard"
 SOURCE=$(git config remote.origin.url | sed -e 's|git@|https://|g' | sed -e 's|github.com:|github.com/|g')
 VERSION=$(git describe --always --tag)
 COMMIT=$(git log --pretty=format:'%H' -n 1)
-echo "{\"name\":\"${NAME}\",\"version\":\"${VERSION}\",\"source\":\"${SOURCE}\",\"commit\":\"${COMMIT}\"}" > version.json
+echo "{\"name\":\"${NAME}\",\"version\":\"${VERSION}\",\"source\":\"${SOURCE}\",\"commit\":\"${COMMIT}\"}" > src/version.json
 
