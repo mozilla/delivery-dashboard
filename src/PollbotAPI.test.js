0e564c48ddcf061863b2eabfe1ee2f583233ecd3
Mathieu Agopian <mathieu@agopian.info>
2017-09-18 14:06:00 +0200

    Test getReleaseInfo

--- src/PollbotAPI.test.js
+++ src/PollbotAPI.test.js
@@ -1,6 +1,6 @@
 /* Functional tests of the Pollbot API */
 
-import {getOngoingVersions} from './PollbotAPI';
+import {getOngoingVersions, getReleaseInfo} from './PollbotAPI';
 
 describe('getOngoingVersions', () => {
   it('retrieves the list of ongoing versions', async () => {
@@ -11,6 +11,29 @@ describe('getOngoingVersions', () => {
       esr: expect.any(String),
       nightly: expect.any(String),
       release: expect.any(String),
+    });
+  });
+});
+
+describe('getReleaseInfo', () => {
+  it('retrieves the release information', async () => {
+    const releaseInfo = await getReleaseInfo('50.0');
+    expect(releaseInfo).toMatchObject({
+      channel: expect.stringMatching(/nightly|beta|release|esr/),
+      checks: expect.arrayContaining([
+        {
+          title: expect.any(String),
+          url: expect.any(String),
+        },
+      ]),
+      product: 'firefox',
+      version: '50.0',
+    });
+    releaseInfo.checks.map(check => {
+      expect(check).toMatchObject({
+        title: expect.any(String),
+        url: expect.any(String),
+      });
     });
   });
 });
