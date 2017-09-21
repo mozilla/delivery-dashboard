725b9ab9bf84aa4ff6ed4e358588740c23a5389b
Mathieu Agopian <mathieu@agopian.info>
2017-09-21 15:25:35 +0200

    Better tests for the fetchPollbotVersion saga

--- src/sagas.test.js
+++ src/sagas.test.js
@@ -1,4 +1,5 @@
 import {call, put, takeEvery} from 'redux-saga/effects';
+import {cloneableGenerator} from 'redux-saga/utils';
 import {getPollbotVersion} from './PollbotAPI';
 import {updatePollbotVersion} from './actions';
 import {REQUEST_POLLBOT_VERSION} from './types';
@@ -6,17 +7,29 @@ import {fetchPollbotVersion, rootSaga} from './sagas';
 
 describe('sagas', () => {
   it('handles requestPollbotVersion', () => {
+    const data = {};
+    data.saga = cloneableGenerator(fetchPollbotVersion)();
+
     const pollbotVersion = {
       name: 'pollbot',
       source: 'https://github.com/mozilla/PollBot.git',
       version: '0.2.1-22-g8e09a0f',
       commit: '8e09a0f8e995344ea24fbb940a6bddc17e0edaed',
     };
-    const saga = fetchPollbotVersion();
-    expect(saga.next().value).toEqual(call(getPollbotVersion));
-    expect(saga.next(pollbotVersion).value).toEqual(
+    expect(data.saga.next().value).toEqual(call(getPollbotVersion));
+
+    // Clone to test success and failure of getPollbotVersion.
+    data.sagaThrow = data.saga.clone();
+
+    expect(data.saga.next(pollbotVersion).value).toEqual(
       put(updatePollbotVersion(pollbotVersion)),
     );
+    expect(data.saga.next().done).toBe(true);
+
+    expect(data.sagaThrow.throw('error').value).toEqual(
+      console.error('Failed getting the pollbot version', 'error'),
+    );
+    expect(data.sagaThrow.next().done).toBe(true);
   });
 });
 
@@ -26,5 +39,6 @@ describe('rootSaga', () => {
     expect(saga.next().value).toEqual([
       takeEvery(REQUEST_POLLBOT_VERSION, fetchPollbotVersion),
     ]);
+    expect(saga.next().done).toBe(true);
   });
 });
