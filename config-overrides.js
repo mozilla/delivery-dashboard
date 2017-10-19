5af771461c12d3c43163e96bc0fec0ce58576010
Mathieu Agopian <mathieu@agopian.info>
2017-10-19 15:23:18 +0200

    Use react-app-rewire-less to allow for ant styling/customization

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,14 @@
 const {injectBabelPlugin} = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
 
 // eslint-disable-next-line
 module.exports = function override(config, env) {
   config = injectBabelPlugin(
-    ['import', {libraryName: 'antd', style: 'css'}],
+    ['import', {libraryName: 'antd', style: true}],
     config,
   );
+  config = rewireLess(config, env, {
+    modifyVars: {'@primary-color': '#1DA57A'},
+  });
   return config;
 };
