12cca2dadca4420f2a051e8a2a8b249dd95e3298
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-11-07 11:26:56 +0100

    Fix tag

--- config-overrides.js
+++ config-overrides.js
@@ -4,7 +4,7 @@ const {injectBabelPlugin} = require('react-app-rewired');
 module.exports = function override(config, env) {
   config = injectBabelPlugin(
     ['import', {libraryName: 'antd', style: 'css'}],
-    config,
+    config
   );
   return config;
 };
