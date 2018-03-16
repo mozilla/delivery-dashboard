f71117e57dd24b2f684aaad19b6fcaf5deae4d29
Mathieu Agopian <mathieu@agopian.info>
2018-03-16 09:58:01 +0100

    Fix flow issues following update to 0.68.0

--- flow-typed/overrides/Notification.js
+++ flow-typed/overrides/Notification.js
@@ -2,24 +2,24 @@
 
 // This file will be automagically used by Flow to type "Notification".
 declare class Notification {
-  constructor(string, ?Object): Notification,
-  permission: 'denied' | 'granted' | 'default',
-  actions: string[],
-  badge: string,
-  body: string,
-  data: Object,
-  dir: 'ltr' | 'rtl',
-  lang: string,
-  tag: string,
-  icon: string,
-  image: string,
-  requireInteraction: boolean,
-  silent: boolean,
-  timestamp: number,
-  title: string,
-  vibrate: number[],
-  onclick(event: Event): void,
-  onerror(event: Event): void,
-  static requestPermission(): Promise<*>,
-  static close(): void,
+  constructor(string, ?Object): Notification;
+  static permission: "denied" | "granted" | "default";
+  actions: string[];
+  badge: string;
+  body: string;
+  data: Object;
+  dir: "ltr" | "rtl";
+  lang: string;
+  tag: string;
+  icon: string;
+  image: string;
+  requireInteraction: boolean;
+  silent: boolean;
+  timestamp: number;
+  title: string;
+  vibrate: number[];
+  onclick(event: Event): void;
+  onerror(event: Event): void;
+  static requestPermission(): Promise<*>;
+  static close(): void;
 }
